19e4b9dbbb95534439451d0986aa06243a771604
Rashy-hub <yagoubi.r@gmail.com>
2024-05-07 19:15:32 +0200

    Exo 3 serie 3 in one line

--- The Field/WEEK-03-04/03.ALGOS-serie3/03.exo.js
+++ The Field/WEEK-03-04/03.ALGOS-serie3/03.exo.js
@@ -1,11 +1,5 @@
-function isSmooth(mySentence) {
-    const initalValue = [true, mySentence.split(' ')[0]]
-
-    return mySentence.split(' ').reduce((acc, word) => {
-        if (word != initalValue[1]) return [acc[0] && word.startsWith(acc[1].charAt(acc[1].length - 1)), word]
-        else return [true, word]
-    }, initalValue)[0]
-}
+const isSmooth = (mySentence) =>
+    mySentence.split(' ').reduce((acc, word) => (word != mySentence.split(' ')[0] ? [acc[0] && word.startsWith(acc[1].charAt(acc[1].length - 1)), word] : [true, word]), [true, mySentence.split(' ')[0]])[0]
 
 console.log(isSmooth('Marta appreciated deep perpendicular right trapezoids')) //➞ true
 console.log(isSmooth('Someone is outside the doorway')) // ➞ false
