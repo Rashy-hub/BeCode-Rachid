6c9d6f7f8caba58b6647275ec22c6d455fbbb40f
Rashy-hub <yagoubi.r@gmail.com>
2024-05-28 09:31:48 +0200

    DOM: exercice 6b done

--- The Hill/WEEK-07/06b.Exercises/app.js
+++ The Hill/WEEK-07/06b.Exercises/app.js
@@ -12,8 +12,21 @@ bold.style.color = 'red'
 
 // the part-2 div should have a darkblue background and a white color.
 bold.closest('.part-2').style.background = 'darkblue'
+bold.closest('.part-2').style.color = 'white'
+
 // The anchor tag in part-2 should be pink
 bold.parentElement.nextElementSibling.style.color = 'pink'
 // all li in part-1 should be orange
+
+bold.closest('.part-2')
+    .previousElementSibling.querySelectorAll('li')
+    .forEach((element) => (element.style.color = 'orange'))
+
 // All a inside the lis should be green
+bold.closest('.part-2')
+    .previousElementSibling.querySelectorAll('li')
+    .forEach((element) => (element.querySelector('a').style.color = 'green'))
+
 // Finally, the h1 should be uppercase.
+
+bold.closest('.part-2').previousElementSibling.querySelector('h1').style.textTransform = 'uppercase'
