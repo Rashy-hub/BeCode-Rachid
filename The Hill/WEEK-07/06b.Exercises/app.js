83e5d1b886edebbe268c5d12f934a3d8b28da9ef
Rashy-hub <yagoubi.r@gmail.com>
2024-05-28 12:24:12 +0200

    DOM : exercices 6b update finish

--- The Hill/WEEK-07/06b.Exercises/app.js
+++ The Hill/WEEK-07/06b.Exercises/app.js
@@ -1,4 +1,5 @@
 console.log('starting javascript')
+//article Exo 1
 const bold = document.querySelector('b')
 
 // Make the following arrangements with only JavaScript : (each line of code should start with the variable bold... The code will be discusting, but.. hey ! it's an exercise)
@@ -30,3 +31,47 @@ bold.closest('.part-2')
 // Finally, the h1 should be uppercase.
 
 bold.closest('.part-2').previousElementSibling.querySelector('h1').style.textTransform = 'uppercase'
+
+/* article Exo2 : 
+
+Use JavaScript to generate exactly 100 buttons and append it to the container div.
+Inside each button there has to be a text "I am button 1", 2, 3, 4 etc.
+Also, change the h1 text to There are a lot of buttons here !
+Lastly... make the buttons appear and the text changes only after 2 seconds (we saw setTimeOut() already) */
+
+const myContainer = document.querySelector('.container')
+const myTitle = document.querySelector('.exo2>h1')
+myTitle.innerText = 'There are a lot of buttons here !'
+
+for (let index = 0; index < 100; index++) {
+    let button = document.createElement('button')
+    button.innerText = `I am button ${index + 1}`
+    button.style.display = 'none'
+    myContainer.appendChild(button)
+    setTimeout(() => {
+        button.style.display = 'block'
+    }, 2000)
+}
+
+/* article Exo3 :
+
+    Use javascript to inverse the highlighted list elements.
+
+*/
+
+const myListItems = document.querySelectorAll('.exo3>ul li')
+myListItems.forEach((item) => item.classList.toggle('highlight'))
+
+/* article Exo4
+    Create an image tag for each Pokemon and append it to the container
+ */
+const myPokeContainer = document.querySelector('.exo4 .container')
+const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+for (let index = 1; index < 151; index++) {
+    const pokemonUrl = baseUrl + `${index}.png`
+    let imagePokemon = document.createElement('img')
+    imagePokemon.setAttribute('src', pokemonUrl)
+    imagePokemon.width = 200
+    imagePokemon.height = 200
+    myPokeContainer.appendChild(imagePokemon)
+}
