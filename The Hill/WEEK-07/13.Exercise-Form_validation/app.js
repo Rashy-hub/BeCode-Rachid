63d6360920ebf3641c3bc376ff644ec384cab287
Rashy-hub <yagoubi.r@gmail.com>
2024-06-03 22:33:35 +0200

    WIP : exo dom

--- The Hill/WEEK-07/13.Exercise-Form_validation/app.js
+++ The Hill/WEEK-07/13.Exercise-Form_validation/app.js
@@ -1,10 +1,3 @@
-const userName = document.getElementById("username")
-userName.focus()
-const inputHandler = (event) => {
-    if (event.target.id === "username") {
-        const regex = /^[a-z]{5,10}$/
-        if (!regex.test(event.target.value)) console.log("username field is not correct")
-    }
-}
+const myForm = document.forms[0]
 
-userName.addEventListener("change", inputHandler)
+userName.focus()
