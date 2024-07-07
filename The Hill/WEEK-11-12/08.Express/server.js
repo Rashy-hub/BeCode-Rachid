e24862a1576b6e558a188641a4b079e12d8a5aef
Rashy-hub <yagoubi.r@gmail.com>
2024-07-07 15:47:01 +0200

    Week 11: express exo done

--- The Hill/WEEK-11-12/08.Express/server.js
+++ The Hill/WEEK-11-12/08.Express/server.js
@@ -6,20 +6,30 @@ const port = 3000
 
 const viewsDir = path.join(__dirname, "views")
 
-app.use(express.static("views", { extensions: ["html"] }))
+//app.use(express.static("views", { extensions: ["html"] }))
 
 // Route for POST request
+app.get("/", (req, res) => {
+    res.sendFile("./views/index.html", { root: __dirname })
+})
+// Manualy send the css file
+app.get("/style.css", (req, res) => {
+    res.sendFile("./views/style.css", { root: __dirname })
+})
+app.get("/about*", (req, res) => {
+    res.sendFile("./views/about.html", { root: __dirname })
+})
+
+app.get("/about-us", (req, res) => {
+    console.log(req.url)
+    res.redirect("/about")
+})
 app.post("/", (req, res) => {
     const data = {
         school: "BeCode",
         year: "2023",
     }
     res.json(data)
-})
-
-app.get("/about-us", (req, res) => {
-    console.log(req.url)
-    res.redirect("/about")
 })
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "./views/404.html"))
